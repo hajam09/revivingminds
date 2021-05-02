8d1c3ac49365fd6d102423f08743860446602ba3
Mohamed Ashraf Hajamohideen <34398691+hajam09@users.noreply.github.com>
2021-05-02 18:48:51 +0100

    display random articles in the main page under THE ARTICLES section

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
 from mainapp.models import Category
 from mainapp.models import Article
+import random
 
 def mainpage(request):
-	return render(request, "mainapp/mainpage.html")
+
+	items = list(Article.objects.filter(show=True))
+	the_articles = random.sample(items, 6)
+
+	context = {
+		"the_articles": the_articles
+	}
+	return render(request, "mainapp/mainpage.html", context)
 
 def article_page(request, category, article):
-	# article page
+
 	all_category = Category.objects.all()
 	recent_posts = Article.objects.all().order_by('-created_at')[:5]
+
 	article = Article.objects.get(
 		slug=article,
 		show=True
 	)
+
 	context = {
 		"all_category": all_category,
 		"article": article,
@@ -27,7 +37,7 @@ def create_article(request):
 	return render(request, "mainapp/create_article.html", context)
 
 def articles(request, category):
-	# list of all the articles
+	# list of all the articles for this category.
 	category = Category.objects.get(slug=category)
 	all_articles = Article.objects.filter(category=category, show=True)
 
