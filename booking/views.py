aa88f160c8b0aea2ffdf7ea3330ce9b796191d59
DESKTOP-VR112O1\Gaming <hajam09@yahoo.com>
2021-05-08 01:56:41 +0100

    using cache to store temp objects to access it on another view

--- booking/views.py
+++ booking/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from accounts.models import Therapist
 from accounts.models import Doctor
+from django.contrib.auth.models import User
 from django.http import Http404
+from booking.models import Session
+from booking.models import Appointment
+from datetime import datetime
+from datetime import timedelta
+from django.shortcuts import redirect
+from django.core.cache import cache
 
 def view_schedule(request, profile):
+	if not request.session.session_key:
+		request.session.save()
+	print(request.session.session_key)
 	profile_obj = None
 	try:
 		profile_obj = Therapist.objects.get(slug=profile)
@@ -18,5 +28,59 @@ def view_schedule(request, profile):
 	if profile_obj == None:
 		raise Http404
 
-	context = {}
-	return render(request, "booking/view_booking_schedule.html", context)
+	if request.method == "POST" and 'create-appointment-for-unauthenticated-user' in request.POST:
+		email = request.POST['email']
+		session_name = request.POST['event-name']
+		if User.objects.filter(email=email).exists():
+			# redirect to login page
+			pass
+
+		# for this doctor or therapist get the consultation session for pricing
+		# redirect user to the payment page
+		if(isinstance(profile_obj, Doctor)):
+			if 'single-event' in request.POST:
+				# booking a single appointment.
+				if(Session.objects.filter(doctor=profile_obj, session_name__iexact=session_name, quantity=1).exists()):
+
+					new_session = Session.objects.filter(doctor=profile_obj, session_name__iexact=session_name)[0]
+					start_time = request.POST['edate']
+					duration = request.POST['duration']
+
+					if not Appointment.objects.filter(doctor=profile_obj, start_time=start_time).exists():
+						new_start_time = datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%S')
+						new_end_time = new_start_time + timedelta(minutes = int(duration))
+						new_appointment = Appointment(
+								doctor=profile_obj,
+								start_time=new_start_time,
+								end_time=new_end_time,
+								session_type=new_session,
+								zoom_link=''
+							)
+						cache.set(request.session.session_key, [new_appointment], 300)
+						# set this object in the cache for 5 minutes.
+						return redirect('payment:landing-page')
+
+		elif(isinstance(profile_obj, Therapist)):
+			if 'single-event' in request.POST:
+				# booking a single appointment.
+				if(Session.objects.filter(therapist=profile_obj, session_name__iexact=session_name, quantity=1).exists()):
+
+					new_session = Session.objects.filter(therapist=profile_obj, session_name__iexact=session_name)[0]
+					start_time = request.POST['edate']
+					duration = request.POST['duration']
+
+					if not Appointment.objects.filter(therapist=profile_obj, start_time=start_time).exists():
+						new_start_time = datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%S')
+						new_end_time = new_start_time + timedelta(minutes = int(duration))
+						new_appointment = Appointment(
+								therapist=profile_obj,
+								start_time=new_start_time,
+								end_time=new_end_time,
+								session_type=new_session,
+								zoom_link=''
+							)
+						cache.set(request.session.session_key, [new_appointment], 300)
+						# set this object in the cache for 5 minutes.
+						return redirect('payment:landing-page')
+
+	return render(request, "booking/view_booking_schedule.html")
--- payment/views.py
+++ payment/views.py
@@ -6,6 +6,7 @@ from booking.models import Session
 from django.http import JsonResponse
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.core.cache import cache
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -20,42 +21,42 @@ class ProductLandingPageView(TemplateView):
 
 	def get_context_data(self, **kwargs):
 		context = super(ProductLandingPageView, self).get_context_data(**kwargs)
+		appointments = cache.get(self.request.session.session_key)
 		context.update({
-			"therapy_session": Session.objects.get(id=1),
+			"appointments": appointments,
 			"STRIPE_PUBLIC_KEY": settings.STRIPE_PUBLIC_KEY
 		})
 		return context
 
 class CreateCheckoutSessionView(View):
 	def post(self, request, *args, **kwargs):
-		print("YES")
-
-		therapy_session_id = self.kwargs["pk"] # to het which booking package patient is going for.
+		
+		therapy_session_id = self.kwargs["pk"]
 		session_type = Session.objects.get(id=therapy_session_id)
 		print(session_type)
 		YOUR_DOMAIN = "http://127.0.0.1:8000"
 
-		checkout_session = stripe.checkout.Session.create(
-			payment_method_types=['card'],
-			line_items=[
-				{
-					'price_data': {
-						'currency': 'gbp',
-						'unit_amount': session_type.cost,
-						'product_data': {
-							'name': session_type.session_name
-						},
-					},
-					'quantity': 1,
-				},
-			],
-			metadata={
-				"session_type_id": session_type.id
-			},
-			mode='payment',
-			success_url=YOUR_DOMAIN + '/payment/success/',
-			cancel_url=YOUR_DOMAIN + '/payment/cancel/',
-		)
+		# checkout_session = stripe.checkout.Session.create(
+		# 	payment_method_types=['card'],
+		# 	line_items=[
+		# 		{
+		# 			'price_data': {
+		# 				'currency': 'gbp',
+		# 				'unit_amount': session_type.cost,
+		# 				'product_data': {
+		# 					'name': session_type.session_name
+		# 				},
+		# 			},
+		# 			'quantity': 1,
+		# 		},
+		# 	],
+		# 	metadata={
+		# 		"session_type_id": session_type.id
+		# 	},
+		# 	mode='payment',
+		# 	success_url=YOUR_DOMAIN + '/payment/success/',
+		# 	cancel_url=YOUR_DOMAIN + '/payment/cancel/',
+		# )
 		return JsonResponse({
-			'session_id': checkout_session.id
+			'session_id': "checkout_session.id"
 		})
